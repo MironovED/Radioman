5a41f6f760c2c947382fe9ea637a34b972865334
Evgeny Mironov <e.d.mironov@yandex.ru>
2022-02-06 21:33:47 +0300

    created class RadioTest

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -99,7 +99,7 @@ class RadioTest {
         int actual = radio.getCurrentRadioStation();
         assertEquals(expected, actual);
     }
-///////         //////  ////    /////
+
     @Test
     void increaseVolume() {
         Radio radio = new Radio();
@@ -143,25 +143,5 @@ class RadioTest {
         int expected = 0;
         assertEquals(expected, actual);
     }
-
-//    @Test
-//    void setCurrentVolume11() {
-//        Radio radio = new Radio();
-//        radio.setCurrentVolume(11);
-//
-//        int actual = radio.getCurrentVolume();
-//        int expected = 0;
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    void setCurrentVolume_1() {
-//        Radio radio = new Radio();
-//        radio.setCurrentVolume(-1);
-//
-//        int actual = radio.getCurrentVolume();
-//        int expected = 0;
-//        assertEquals(expected, actual);
-//    }
 
 }
